27f90fb830132349c9fe759d8a84f010d1429549
Susmita Saha <52951838+susmitasaha44@users.noreply.github.com>
2021-09-26 00:20:03 +0600

    Add files via upload

---
+++ Final/Final/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cover_image_loc = @"C:\Users\USER\Desktop\img\fw.png";
+            Bitmap newBitmap = new Bitmap(cover_image_loc);
+            for (int y = 0; y < 1; y++)
+            {
+                for (int x = 0; x < newBitmap.Width; x++)
+                {
+
+                    newBitmap.SetPixel(x, y, Color.Yellow);
+                }
+            }
+            newBitmap.Save(@"C:\Users\USER\Desktop\img\fw2.png");
+            MessageBox.Show("Complete!!");
+
+        }
+    }
+}
